05cb829a97379fac66e897200e36a7d4ed5657cf
zxcvvxcz <48852336+zxcvvxcz@users.noreply.github.com>
2019-12-28 16:22:47 +0900

    Add files via upload

---
+++ Rozetta_skel/rozetta.ml
@@ -0,0 +1,67 @@
+(*
+ * SNU 4190.310 Programming Languages 
+ * Homework "Rozetta" Skeleton
+ *)
+let num_ref = ref 0 
+let trans_v : Sm5.value -> Sonata.value = function
+  | Sm5.Z z  -> Sonata.Z z
+  | Sm5.B b  -> Sonata.B b
+  | Sm5.L _ -> raise (Sonata.Error "Invalid input program : pushing location")
+  | Sm5.Unit -> Sonata.Unit
+  | Sm5.R _ -> raise (Sonata.Error "Invalid input program : pushing record")
+
+(* TODO : complete this function *)
+let rec trans_obj : Sm5.obj -> Sonata.obj = function
+  | Sm5.Val v -> Sonata.Val (trans_v v)
+  | Sm5.Id id -> Sonata.Id id
+  | Sm5.Fn (arg, command) -> 
+    Sonata.Fn (arg, (trans' command) @
+      Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD :: Sonata.PUSH (Sonata.Val (Sonata.Z 1)) :: Sonata.SUB ::
+      Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.STORE :: Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD :: 
+      Sonata.PUSH (Sonata.Id "#cont_loc") :: Sonata.ADD :: Sonata.LOAD ::
+      Sonata.PUSH (Sonata.Val Sonata.Unit) :: Sonata.MALLOC :: Sonata.CALL :: [])
+
+(* TODO : complete this function *)
+and trans' : Sm5.command -> Sonata.command = 
+  function
+  | Sm5.PUSH obj :: cmds -> Sonata.PUSH (trans_obj obj) :: (trans' cmds)
+  | Sm5.POP :: cmds -> Sonata.POP :: (trans' cmds)
+  | Sm5.STORE :: cmds -> Sonata.STORE :: (trans' cmds)
+  | Sm5.LOAD :: cmds -> Sonata.LOAD :: (trans' cmds)
+  | Sm5.JTR (c1, c2) :: cmds ->  Sonata.JTR (trans' (c1 @ cmds), trans' (c2 @ cmds)) :: []
+    
+  | Sm5.MALLOC :: cmds -> Sonata.MALLOC :: (trans' cmds)
+  | Sm5.BOX z :: cmds -> Sonata.BOX z :: (trans' cmds)
+  | Sm5.UNBOX id :: cmds -> Sonata.UNBOX id :: (trans' cmds)
+  | Sm5.BIND id :: cmds -> Sonata.BIND id :: (trans' cmds)
+  | Sm5.UNBIND :: cmds -> Sonata.UNBIND :: (trans' cmds)
+  | Sm5.GET :: cmds -> Sonata.GET :: (trans' cmds)
+  | Sm5.PUT :: cmds -> Sonata.PUT :: (trans' cmds)
+  | Sm5.CALL :: cmds -> 
+    Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD :: Sonata.PUSH (Sonata.Val (Sonata.Z 0)) :: Sonata.EQ :: 
+    Sonata.JTR (Sonata.PUSH (Sonata.Fn ("#cont_a", trans' cmds)) :: [],
+      Sonata.PUSH (Sonata.Fn ("#cont_a", (trans' cmds) @
+        Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD :: Sonata.PUSH (Sonata.Val (Sonata.Z 1)) ::
+        Sonata.SUB :: Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.STORE ::
+        Sonata.PUSH (Sonata.Id "#cont_loc") :: Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD ::
+        Sonata.ADD :: Sonata.LOAD :: Sonata.PUSH (Sonata.Val Sonata.Unit) :: Sonata.MALLOC :: Sonata.CALL :: [])) :: []) :: 
+    Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD :: Sonata.PUSH (Sonata.Id "#cont_loc") :: Sonata.ADD :: 
+    Sonata.STORE :: Sonata.PUSH (Sonata.Val (Sonata.Z 1)) :: Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.LOAD :: 
+    Sonata.ADD :: Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.STORE :: Sonata.CALL :: []
+    
+  | Sm5.ADD :: cmds -> Sonata.ADD :: (trans' cmds)
+  | Sm5.SUB :: cmds -> Sonata.SUB :: (trans' cmds)
+  | Sm5.MUL :: cmds -> Sonata.MUL :: (trans' cmds)
+  | Sm5.DIV :: cmds -> Sonata.DIV :: (trans' cmds)
+  | Sm5.EQ :: cmds -> Sonata.EQ :: (trans' cmds)
+  | Sm5.LESS :: cmds -> Sonata.LESS :: (trans' cmds)
+  | Sm5.NOT :: cmds -> Sonata.NOT :: (trans' cmds)
+  | [] -> []
+
+(* TODO : complete this function *)
+let trans : Sm5.command -> Sonata.command = fun command -> 
+  Sonata.MALLOC :: Sonata.BIND "#cont_loc" :: 
+  Sonata.PUSH (Sonata.Fn ("#cont_a", [Sonata.PUSH (Sonata.Val Sonata.Unit)])) :: 
+  Sonata.PUSH (Sonata.Id "#cont_loc") :: Sonata.STORE :: 
+  Sonata.MALLOC :: Sonata.BIND "#fun_depth" :: Sonata.PUSH (Sonata.Val (Sonata.Z 0)) :: 
+  Sonata.PUSH (Sonata.Id "#fun_depth") :: Sonata.STORE :: trans' command 
